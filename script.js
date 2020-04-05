99c05905312b0553034e734126edced2301f310b
igor77712 <igortym777@gmail.com>
2020-04-05 12:19:38 +0300

    feat: draw keyboard

---
+++ script.js
@@ -0,0 +1,188 @@
+const a = [];
+
+a[0]=[
+    {className:"Backquote",eng:{caseDown:"`",caseUp:"~"},rus:{caseDown:"ё",caseUp:"Ё"}},
+    {className:"Digit1",eng:{caseDown:"1",caseUp:"!"},rus:{caseDown:"1",caseUp:"!"}},
+    {className:"Digit2",eng:{caseDown:"2",caseUp:"@"},rus:{caseDown:"2",caseUp:'"'}},
+    {className:"Digit3",eng:{caseDown:"3",caseUp:"#"},rus:{caseDown:"3",caseUp:"№"}},
+    {className:"Digit4",eng:{caseDown:"4",caseUp:"$"},rus:{caseDown:"4",caseUp:";"}},
+    {className:"Digit5",eng:{caseDown:"5",caseUp:"%"},rus:{caseDown:"5",caseUp:"%"}},
+    {className:"Digit6",eng:{caseDown:"6",caseUp:"^"},rus:{caseDown:"6",caseUp:":"}},
+    {className:"Digit7",eng:{caseDown:"7",caseUp:"&"},rus:{caseDown:"7",caseUp:"?"}},
+    {className:"Digit8",eng:{caseDown:"8",caseUp:"*"},rus:{caseDown:"8",caseUp:"*"}},
+    {className:"Digit9",eng:{caseDown:"9",caseUp:"("},rus:{caseDown:"9",caseUp:"("}},
+    {className:"Digit0",eng:{caseDown:"0",caseUp:")"},rus:{caseDown:"0",caseUp:")"}},
+    {className:"Minus",eng:{caseDown:"-",caseUp:"_"},rus:{caseDown:"-",caseUp:"_"}},
+    {className:"Equal",eng:{caseDown:"=",caseUp:"+"},rus:{caseDown:"=",caseUp:"+"}},
+    {className:"Backspace",eng:{caseDown:"Backspace",caseUp:"Backspace"},rus:{caseDown:"Backspace",caseUp:"Backspace"}}];
+
+a[1]=[
+    {className:"Tab",eng:{caseDown:"Tab",caseUp:"Tab"},rus:{caseDown:"Tab",caseUp:"Tab"}},
+    {className:"KeyQ",eng:{caseDown:"q",caseUp:"Q"},rus:{caseDown:"й",caseUp:"Й"}},
+    {className:"KeyW",eng:{caseDown:"w",caseUp:"W"},rus:{caseDown:"ц",caseUp:"Ц"}},
+    {className:"KeyE",eng:{caseDown:"e",caseUp:"E"},rus:{caseDown:"у",caseUp:"У"}},
+    {className:"KeyR",eng:{caseDown:"r",caseUp:"R"},rus:{caseDown:"к",caseUp:"К"}},
+    {className:"KeyT",eng:{caseDown:"t",caseUp:"T"},rus:{caseDown:"е",caseUp:"Е"}},
+    {className:"KeyY",eng:{caseDown:"y",caseUp:"Y"},rus:{caseDown:"н",caseUp:"Н"}},
+    {className:"KeyU",eng:{caseDown:"u",caseUp:"U"},rus:{caseDown:"г",caseUp:"Г"}},
+    {className:"KeyI",eng:{caseDown:"i",caseUp:"I"},rus:{caseDown:"ш",caseUp:"Ш"}},
+    {className:"KeyO",eng:{caseDown:"o",caseUp:"O"},rus:{caseDown:"щ",caseUp:"Щ"}},
+    {className:"KeyP",eng:{caseDown:"p",caseUp:"P"},rus:{caseDown:"з",caseUp:"З"}},
+    {className:"BracketLeft",eng:{caseDown:"[",caseUp:"{"},rus:{caseDown:"х",caseUp:"Х"}},
+    {className:"BracketRight",eng:{caseDown:"]",caseUp:"}"},rus:{caseDown:"ъ",caseUp:"Ъ"}},
+    {className:"Backslash",eng:{caseDown:"\\",caseUp:"|"},rus:{caseDown:"\\",caseUp:"/"}},
+    {className:"Delete",eng:{caseDown:"Del",caseUp:"Del"},rus:{caseDown:"Del",caseUp:"Del"}}];
+
+a[2]=[
+    {className:"CapsLock",eng:{caseDown:"CapsLock",caseUp:"CapsLock"},rus:{caseDown:"CapsLock",caseUp:"CapsLock"}},
+    {className:"KeyA",eng:{caseDown:"a",caseUp:"A"},rus:{caseDown:"ф",caseUp:"Ф"}},
+    {className:"KeyS",eng:{caseDown:"s",caseUp:"S"},rus:{caseDown:"ы",caseUp:"Ы"}},
+    {className:"KeyD",eng:{caseDown:"d",caseUp:"D"},rus:{caseDown:"в",caseUp:"В"}},
+    {className:"KeyF",eng:{caseDown:"f",caseUp:"F"},rus:{caseDown:"а",caseUp:"А"}},
+    {className:"KeyG",eng:{caseDown:"g",caseUp:"G"},rus:{caseDown:"п",caseUp:"П"}},
+    {className:"KeyH",eng:{caseDown:"h",caseUp:"H"},rus:{caseDown:"р",caseUp:"Р"}},
+    {className:"KeyJ",eng:{caseDown:"j",caseUp:"J"},rus:{caseDown:"о",caseUp:"О"}},
+    {className:"KeyK",eng:{caseDown:"k",caseUp:"K"},rus:{caseDown:"л",caseUp:"Л"}},
+    {className:"KeyL",eng:{caseDown:"l",caseUp:"L"},rus:{caseDown:"д",caseUp:"Д"}},
+    {className:"Semicolon",eng:{caseDown:";",caseUp:":"},rus:{caseDown:"ж",caseUp:"Ж"}},
+    {className:"Quote",eng:{caseDown:"'",caseUp:'"'},rus:{caseDown:"э",caseUp:"Э"}},
+    {className:"Enter",eng:{caseDown:"Enter",caseUp:"Enter"},rus:{caseDown:"Enter",caseUp:"Enter"}}];
+
+a[3]=[
+    {className:"ShiftLeft",eng:{caseDown:"Shift",caseUp:"Shift"},rus:{caseDown:"Shift",caseUp:"Shift"}},
+    {className:"KeyZ",eng:{caseDown:"z",caseUp:"Z"},rus:{caseDown:"я",caseUp:"Я"}},
+    {className:"KeyX",eng:{caseDown:"x",caseUp:"X"},rus:{caseDown:"ч",caseUp:"Ч"}},
+    {className:"KeyC",eng:{caseDown:"c",caseUp:"C"},rus:{caseDown:"с",caseUp:"С"}},
+    {className:"KeyV",eng:{caseDown:"v",caseUp:"V"},rus:{caseDown:"м",caseUp:"М"}},
+    {className:"KeyB",eng:{caseDown:"b",caseUp:"B"},rus:{caseDown:"и",caseUp:"И"}},
+    {className:"KeyN",eng:{caseDown:"n",caseUp:"N"},rus:{caseDown:"т",caseUp:"Т"}},
+    {className:"KeyM",eng:{caseDown:"m",caseUp:"M"},rus:{caseDown:"ь",caseUp:"Ь"}},
+    {className:"Comma",eng:{caseDown:",",caseUp:"<"},rus:{caseDown:"б",caseUp:"Б"}},
+    {className:"Period",eng:{caseDown:".",caseUp:">"},rus:{caseDown:"ю",caseUp:"Ю"}},
+    {className:"Slash",eng:{caseDown:"/",caseUp:"?"},rus:{caseDown:".",caseUp:","}},
+    {className:"ArrowUp",eng:{caseDown:"▲",caseUp:"▲"},rus:{caseDown:"▲",caseUp:"▲"}},
+    {className:"ShiftRight",eng:{caseDown:"Shift",caseUp:"Shift"},rus:{caseDown:"Shift",caseUp:"Shift"}}];
+    
+a[4]=[
+    {className:"ControlLeft",eng:{caseDown:"Ctrl",caseUp:"Ctrl"},rus:{caseDown:"Ctrl",caseUp:"Ctrl"}},
+    {className:"MetaLeft",eng:{caseDown:"Win",caseUp:"Win"},rus:{caseDown:"Win",caseUp:"Win"}},
+    {className:"AltLeft",eng:{caseDown:"Alt",caseUp:"Alt"},rus:{caseDown:"Alt",caseUp:"Alt"}},
+    {className:"Space",eng:{caseDown:" ",caseUp:" "},rus:{caseDown:" ",caseUp:" "}},
+    {className:"AltRight",eng:{caseDown:"Alt",caseUp:"Alt"},rus:{caseDown:"Alt",caseUp:"Alt"}},
+    {className:"ArrowLeft",eng:{caseDown:"◄",caseUp:"◄"},rus:{caseDown:"◄",caseUp:"◄"}},
+    {className:"ArrowDown",eng:{caseDown:"▼",caseUp:"▼"},rus:{caseDown:"▼",caseUp:"▼"}},
+    {className:"ArrowRight",eng:{caseDown:"►",caseUp:"►"},rus:{caseDown:"►",caseUp:"►"}},
+    {className:"ControlRight",eng:{caseDown:"Ctrl",caseUp:"Ctrl"},rus:{caseDown:"Ctrl",caseUp:"Ctrl"}}];
+
+const specialKey=["Backspace","Tab","Enter","CapsLock","ShiftLeft","ShiftRight","AltLeft","AltRight","ControlLeft","ControlRight","MetaLeft","Delete", "Space"];
+
+
+    
+
+const BODY =  document.body;
+const WRAPPER = document.createElement('div');
+
+WRAPPER.setAttribute('class', 'wrapper');
+WRAPPER.innerHTML = `
+        <textarea name="text" id="text" class="textarea-keyboard" cols="100" rows="10" autofocus></textarea>
+        <div class="keyboard" id="keyboard"></div>
+`
+BODY.append(WRAPPER);
+
+const KEYBOARD = document.querySelector('#keyboard');
+const TEXTAREA = document.querySelector('textarea');
+
+const drawKey = (key, type) => {
+    return `
+        <button class="${type} ${key.className} key">
+            ${key.eng.caseDown}
+        </button>
+   `;
+}
+
+const drawSpecialKey = (key) => {
+    if(["Backspace","Enter","CapsLock","ShiftLeft", "ShiftRight"].includes(key.className)) {
+        KEYBOARD.insertAdjacentHTML("beforeend", drawKey(key, 'key__special-double'));
+    } else {
+        if(key.className === 'Space'){
+            KEYBOARD.insertAdjacentHTML("beforeend", drawKey(key, 'key__special-space'));
+        } else {
+            KEYBOARD.insertAdjacentHTML("beforeend", drawKey(key, 'key__special-simple'));
+        }
+    }
+    
+}
+
+const drawKeyboard = (keys) => {
+    for(let row of keys){
+        for(let key of row){
+            if(specialKey.includes(key.className)){
+                drawSpecialKey(key);
+            } else {
+                KEYBOARD.insertAdjacentHTML("beforeend", drawKey(key, 'key__simple'));
+            }
+            
+        }
+    }
+} 
+
+drawKeyboard(a);
+
+// Events
+const keyDownSpecialKey = (e) => {
+    console.log(e.target)
+    if (Array.from(e.target.classList).includes('Space')){
+        TEXTAREA.value += " ";
+    }
+
+    if (Array.from(e.target.classList).includes('Backspace')) {
+        TEXTAREA.value = TEXTAREA.value.slice(0, -1);
+    }
+}
+
+const clickButtonMouse = (e) => {
+    if(
+        e.target.className != e.currentTarget.className && 
+        !Array.from(e.target.classList).includes('key__special-double') && 
+        !Array.from(e.target.classList).includes('key__special-simple') && 
+        !Array.from(e.target.classList).includes('key__special-space')) {
+
+        TEXTAREA.value += e.target.innerHTML.trim();
+        TEXTAREA.focus();
+    } else {
+        keyDownSpecialKey(e)
+        TEXTAREA.focus();
+    }
+}
+
+const keyDownA4 = (e) => {
+    TEXTAREA.focus();
+    let down = document.querySelector('.'+e.code);
+    down.classList.add('key__down');
+}
+
+const keyUpA4 = (e) => {
+    TEXTAREA.focus();
+    let up = document.querySelector('.'+e.code);
+    up.classList.remove('key__down');
+}
+
+KEYBOARD.addEventListener('click', clickButtonMouse);
+BODY.addEventListener('keyup', keyUpA4);
+BODY.addEventListener('keydown', keyDownA4);
+
+// const test = (e) => {
+//     // console.log('e.target: ', e.target)
+//     // console.log('e.target.classList: ',  e.target.classList)
+//     // console.log('textarea.value', TEXTAREA.value)
+// }
+
+// KEYBOARD.addEventListener('click', test);
+
+// const testKeyPress = (e) => {
+//     console.log(e)
+//     console.log('e.code: ', e.code)
+//     console.log('e.key: ', e.key)
+// }
+
+// BODY.addEventListener('keydown', testKeyPress);
